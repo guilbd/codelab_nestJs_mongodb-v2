dcb5ec6fcacabe3b197fb16d9e084243f4ca546e
guilbd@hotmail.com <guilbd@hotmail.com>
2021-09-21 22:41:29 -0300

    .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,15 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const dbChar = process.env.DB_CHAR;
+
 @Module({
   imports: [
     UsersModule,
     MongooseModule.forRoot(
-      'mongodb+srv://guilbd:NyPFum4per1XdsWJ@cluster0.jq6gs.mongodb.net/test',
+      `mongodb+srv://${dbUser}:${dbPassword}@cluster0.${dbChar}.mongodb.net/test`,
     ),
   ],
   controllers: [],
